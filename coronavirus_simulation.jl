e1a77a434b09d272d887fd16b8ebfbe6ac5ba5a8
ryo-ARAKI <r_araki@fm.me.es.osaka-u.ac.jp>
2020-03-19 08:46:19 +0900

    dev: refactoring

--- coronavirus_simulation.jl
+++ coronavirus_simulation.jl
@@ -65,13 +65,14 @@ using Distributions
             ptcl[itr_ptcl].pos_y = rand(Uniform(0.0, param.y_range))
             ptcl[itr_ptcl].vel_x = rand(Normal(param.vel_mean, param.vel_σ))  # Gaussian distribution
             ptcl[itr_ptcl].vel_y = rand(Normal(param.vel_mean, param.vel_σ))
+            ptcl[itr_ptcl].t_ifcn = 0
+            ptcl[itr_ptcl].past_ifcn = 0
             ptcl[itr_ptcl].status = "not_infected"  # Initially not infected
-            if itr_ptcl <= num_infected_init  # Some particles are initially infected
+            # Some particles are initially infected
+            if itr_ptcl <= num_infected_init
                 ptcl[itr_ptcl].status = "infected"
                 ptcl[itr_ptcl].past_ifcn = 1
             end
-            ptcl[itr_ptcl].t_ifcn = 0
-            ptcl[itr_ptcl].past_ifcn = 0
         end
         # ptcl.pos_x .= rand(Uniform(0.0, param.x_range))  # ERROR: LoadError: type Array has no field pos_x
     end
@@ -199,14 +200,12 @@ using Distributions
                 end
             end
 
-            ptcl[itr_ptcl].pos_x = x
-            ptcl[itr_ptcl].pos_y = y
             ptcl[itr_ptcl].status = s
             ptcl[itr_ptcl].t_ifcn = t
             ptcl[itr_ptcl].past_ifcn = p
         end
 
-        # Update position of all particles
+        # Update position and velocity of all particles
         for itr_ptcl = 1:param.num_particles
             x = ptcl[itr_ptcl].pos_x
             y = ptcl[itr_ptcl].pos_y
@@ -237,9 +236,9 @@ using Distributions
 
     """
     Count number of
-    - Number of not infected particles
-    - Number of infected particles
-    - Number of recovered particles
+    - not infected particles
+    - infected particles
+    - recovered particles
     """
     function count_status(param, ptcl)
         num_not_infected, num_infected, num_recovered = 0, 0, 0
@@ -311,7 +310,7 @@ module Output
             x_recovered, y_recovered,
             aspect_ratio = 1,
             markercolor = :orange,
-            label = "recovered",
+            label = "Recovered",
             xlims = (0.0, param.x_range),
             ylims = (0.0, param.y_range),
             axis = nothing,
